412ea77d08f324ef1b34a1a97c0a5ad9e4376bfe
Ammar <ammar@shopgo.me>
2014-06-29 14:36:35 +0300

    Updated upgrade script.

--- app/code/community/Shopgo/AramexShipping/sql/aramexshipping_setup/mysql4-upgrade-1.0.2-1.0.7.php
+++ app/code/community/Shopgo/AramexShipping/sql/aramexshipping_setup/mysql4-upgrade-1.0.2-1.0.7.php
@@ -4,12 +4,25 @@ $installer = $this;
 
 $installer->startSetup();
 
-$installer->run("
+$installer->getConnection()
+    ->addColumn(
+        $this->getTable('aramex_shipping_suppliers'),
+        'product_type',
+        "varchar (30) NULL AFTER `account_pin`"
+    );
 
-ALTER TABLE {$this->getTable('aramex_shipping_suppliers')} ADD `product_type` varchar(30) NULL AFTER `account_pin`;
-ALTER TABLE {$this->getTable('aramex_shipping_suppliers')} ADD `dom_product_type` varchar(30) NULL AFTER `product_type`;
-ALTER TABLE {$this->getTable('aramex_shipping_suppliers')} ADD `dom_customs_value` smallint(6) NULL AFTER `dom_product_type`;
+$installer->getConnection()
+    ->addColumn(
+        $this->getTable('aramex_shipping_suppliers'),
+        'dom_product_type',
+        "varchar (30) NULL AFTER `product_type`"
+    );
 
-    ");
+$installer->getConnection()
+    ->addColumn(
+        $this->getTable('aramex_shipping_suppliers'),
+        'dom_customs_value',
+        "smallint(6) NULL AFTER `dom_product_type`"
+    );
 
 $installer->endSetup();
